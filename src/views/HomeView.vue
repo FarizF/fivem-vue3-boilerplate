13fbdfca94169a56ec516b702fdff9fe056777b8
FARIZ-PC\fariz <fariz_fakkel@hotmail.com>
2022-04-21 18:23:03 +0200

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue';
+
+let data: null | string;
+
+window.addEventListener('message', event => {
+  console.log(event.data);
+  data = event.data;
+});
+</script>
+
+<template>
+  <main>
+    {{ data }}
+    <TheWelcome />
+  </main>
+</template>
