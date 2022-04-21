406611e407700626e457b9d9f00414c8929739fc
FarizF <fariz_fakkel@hotmail.com>
2022-04-21 19:27:13 +0200

    Added stock fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'FiveM Vue Boilerplate'
+description 'FiveM Vue Boilerplate'
+author 'Fariz Fakkel (Fariz F)'
+version '1.0.0'
+
+client_script 'client/*.client.js'
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/assets/*',
+}
